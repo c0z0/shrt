e9641bd4d07401f9c41225a634ce45c6b4e5c81a
Cosmin Serdean <cosmoserdean@gmail.com>
2018-01-29 15:35:37 +0200

    Change router and add CORS support

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 const { json, send } = require('micro')
-const url = require('url')
 const shortid = require('shortid')
 const redirect = require('./redirect')
+const router = require('router')()
 
+const cors = require('./cors')()
 const { addUrl, getUrl } = require(process.env.MONGO_URL
 	? './db'
 	: './fsDataLayer')
 
-const addRoute = /\/sh\/?/g
+router.get('/', (req, res) => {
+	send(res, 200, 'SHRT API')
+})
 
-module.exports = async (req, res) => {
-	const parsedUrl = url.parse(req.url)
+router.get('/:id', async (req, res) => {
+	const foundUrl = await getUrl(req.params.id)
 
-	if (addRoute.test(parsedUrl.pathname) && req.method === 'POST') {
-		const data = await json(req)
-
-		if (!data || !data.url) return send(res, 400, 'URL field not present')
-
-		const newUrl = await addUrl(data.url)
-
-		return send(res, 200, newUrl)
-	}
-
-	if (shortid.isValid(parsedUrl.pathname.slice(1)) && req.method === 'GET') {
-		const foundUrl = await getUrl(parsedUrl.pathname.slice(1))
-
-		console.log(foundUrl)
-
-		if (foundUrl) return redirect(res, 308, foundUrl.url)
-	}
-
-	if (parsedUrl.pathname === '/' && req.method === 'GET') {
-		send(res, '200', 'SHRT API')
-	}
-
+	if (foundUrl) return redirect(res, 308, foundUrl.url)
 	send(res, 404, 'Not Found')
-}
+})
+
+router.post('/sh', async (req, res) => {
+	const data = await json(req)
+
+	if (!data || !data.url) return send(res, 400, 'URL field not present')
+
+	const newUrl = await addUrl(data.url)
+
+	send(res, 200, newUrl)
+})
+
+module.exports = cors((req, res) => router(req, res, () => {}))
